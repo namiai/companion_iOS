cced48338bce40c1ce42e81284f38989ad0fae17
Ilya <uin153974748@gmail.com>
2023-02-23 11:08:12 +0300

    Initial Commit

---
+++ nami companion/nami_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  nami_companionApp.swift
+//  nami companion
+//
+//  Created by Yachin Ilya on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct nami_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
