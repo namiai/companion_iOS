02898936a74cccb11ebf395a304c45aa3680586e
Ilya <uin153974748@gmail.com>
2023-10-18 14:01:04 +0800

    use latest framework

--- nami companion/PairingManager.swift
+++ nami companion/PairingManager.swift
@@ -11,7 +11,9 @@ final class PairingManager {
     
     init(sessionCode: String) throws {
         do {
-            pairing = try NamiPairing<ViewsContainer>(sessionCode: sessionCode)
+            // This init is called passing all the parameters, but the same defaults for `wifiStorage` and `threadDatasetStore` would be set implicitly.
+            // Could be initialized as `try NamiPairing<ViewsContainer>(sessionCode: sessionCode)`.
+            pairing = try NamiPairing<ViewsContainer>(sessionCode: sessionCode, wifiStorage: InMemoryWiFiStorage(), threadDatasetStore: InMemoryThreadDatasetStorage.self)
             setupSubscription()
         } catch {
             if let e = error as? NetworkError {
@@ -45,7 +47,9 @@ final class PairingManager {
                 pairing.startPairing(
                     roomId: roomId,
                     pairingSteps: ViewsContainer(),
-                    pairingParameters: bssidPin == nil ? Tomonari.PairingParameters() : Tomonari.PairingParameters(bssid: bssidPin!)
+                    // Plaese notice the BSSID pin is passed here to limit the WIFi networks search.
+                    // Here it is in form of `[UInt8]` but also could be `Data` or ":"-separated MAC-formatted `String`.
+                    pairingParameters: bssidPin == nil ? NamiPairing.PairingParameters() : NamiPairing.PairingParameters(bssid: bssidPin!)
                 )
             )
         } catch {
